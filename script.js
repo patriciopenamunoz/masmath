36a6a5d60db4a2db1c81b63fb6617c6cc6c66fcb
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2020-02-01 16:48:47 -0300

    INITIAL COMMIT

---
+++ script.js
@@ -0,0 +1,22 @@
+var answer = 0;
+
+document.getElementById("answer").addEventListener("keyup", validateInput);
+
+function setQuestion() {
+  let num1 = Math.round(Math.random() * (9 - 1) + 1);
+  let num2 = Math.round(Math.random() * (9 - 1) + 1);
+
+  document.getElementById("question").innerHTML = `${num1} x ${num2}`;
+  answer = num1 * num2;
+}
+
+function validateInput(event) {
+  if (document.getElementById("answer").value == answer) {
+    document.getElementById("answer").value = "";
+    setQuestion();
+  }
+}
+
+window.onload = function() {
+  setQuestion();
+};
